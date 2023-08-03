36930e8e25d8ba426b592f4c624a1e9f9b9667d0
SinthuPriya <sinthuja.engg@gmail.com>
2023-08-03 12:03:16 +0530

    Customized Message

---
+++ src/org/hexa/dev/login/JavaSample.java
@@ -0,0 +1,17 @@
+package org.hexa.dev.login;
+
+public class JavaSample {
+	
+   public void sample() {
+	   System.out.println("sinthuja");
+   }
+	
+	
+	public static void main(String[] args) {
+
+		 JavaSample obj = new JavaSample();
+		 obj.sample();
+		
+	}
+
+}
